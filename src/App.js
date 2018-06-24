8e7ce5cd535a9c7e4485fd61f364d3a12a3ce20e
Blooot <dalehamilton@trentu.ca>
2018-06-24 01:25:07 -0400

    finish final touches

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -13,7 +14,7 @@ class App extends Component {
         }
     }
     onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+        this.setState({ searchfield: event.target.value })
     }
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
@@ -21,7 +22,7 @@ class App extends Component {
         })
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
