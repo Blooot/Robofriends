238e8667028a761ea46c4a368f48556b19066957
Blooot <dalehamilton@trentu.ca>
2018-07-11 18:35:46 -0400

    Add redux

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Robot friends are more accepting than real friends. I used this project to learn about React and es6.
+Robot friends are more accepting than real friends. I used this project to learn about React, es6, and with this latest update, redux!
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleWare from 'redux-thunk';
 import App from './containers/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare, logger))
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
